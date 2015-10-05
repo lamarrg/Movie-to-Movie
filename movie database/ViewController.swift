80eb7c30ca966c5a88d88bf7804c8711fa86ca0f
Lamar Greene <Lamar@lamars-macbook-pro.local>
2015-10-04 20:07:17 -0500

    Initial Commit

---
+++ movie database/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movie database
+//
+//  Created by Lamar Greene on 10/4/15.
+//  Copyright © 2015 Lamar Greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
